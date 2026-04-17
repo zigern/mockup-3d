da5ed7f088d28895f028d26b19568cf292570fc7
3DMOCKUP <joaogirao98@gmail.com>
2026-04-17 12:06:46 +0100

    Add separate homepage and dedicated studio page

---
+++ vite.config.cjs
@@ -0,0 +1,12 @@
+﻿const { resolve } = require('path');
+
+module.exports = {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        studio: resolve(__dirname, 'studio.html'),
+      },
+    },
+  },
+};
