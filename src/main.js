acae01ccdb434fe60d8e5cefb30740374b7b3684
3DMOCKUP <joaogirao98@gmail.com>
2026-04-17 11:11:23 +0100

    Add revert button next to background upload

--- src/main.js
+++ src/main.js
@@ -272,6 +272,7 @@ bindSlider('puffPrint', 'puffVal', (value) => {
 });
 
 const bgImageUpload = document.getElementById('bgImageUpload');
+const bgReset = document.getElementById('bgReset');
 const bgColor = document.getElementById('bgColor');
 const bgPresetGroup = document.getElementById('bgPresetGroup');
 const bgGrayStrip = document.getElementById('bgGrayStrip');
@@ -318,6 +319,14 @@ bgImageUpload.addEventListener('change', (event) => {
   currentBackgroundObjectUrl = imageUrl;
   container.style.background = `center / cover no-repeat url("${imageUrl}")`;
   setActiveBackgroundThumb('');
+  setActiveGrayChip('');
+});
+
+bgReset.addEventListener('click', () => {
+  clearBackgroundObjectUrl();
+  applyBackgroundPreset('dark');
+  if (bgColor) bgColor.value = '#1f1f24';
+  setActiveBackgroundThumb('[data-bg-preset="dark"]');
   setActiveGrayChip('');
 });
 
